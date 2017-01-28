6b2e70804bdae5b7b3882245d220d2c8dcbc4243
JonyLucas <jony.lucas.br13@gmail.com>
2017-01-27 21:31:33 -0300

    Add files via upload

--- algosort.cpp
+++ algosort.cpp
@@ -2,118 +2,222 @@
 *
 *   Aluno: João Lucas Fabião Amorim
 *   Matricula: 11409560
-*   APA 2016.2 - Trabalho 01 / Unidade 01
+*   APA 2016.2 - Trabalho 02 / Unidade 01
 *   Descrição: Trabalho de APA que consiste em implementar e testar os seguintes algoritmos de ordenação - Insertion Sort, Selection Sort,
 *   Quick Sort, Merge Sort, Heap Sort.
 *
 **/
 
 #include <iostream>
-#include <vector>
 #include <fstream>
 #include <cstdlib>
+#include <time.h>
 
 using namespace std;
 
-void insertion_sort(vector<int>& vetor);
-void selection_sort(vector<int>& vetor);
-void quick_sort(vector<int>& vetor, int ini, int fim);
-void merge_sort(vector<int>& vetor, int ini, int fim);
-void merge(vector<int>& vetor, int ini, int meio, int fim);
-void heap_sort(vector<int>& vetor);
-void max_heap(vector<int>& vetor, int ind, int n);
-void exibe_lista(vector<int> vetor);
-bool ordenado(vector<int> vetor);
+void insertion_sort(int vetor[], int size);
+void selection_sort(int vetor[], int size);
+void quick_sort(int vetor[],int ini, int fim);
+void merge_sort(int vetor[], int size, int ini, int fim);
+void merge(int vetor[], int size, int ini, int meio, int fim);
+void heap_sort(int vetor[], int size);
+void max_heap(int vetor[], int ind, int n);
+void exibe_lista(int vetor[], int size);
+bool ordenado(int vetor[], int size);
+
+ofstream osfile("schedule.csv", ios::app);
 
 int main(int argc, char* argv[]) {
 
-    int num, tam;
-    
-    vector<int> vetor;
-    
-    cin >> tam; //O primeiro elemento corresponde ao número de elementos do vetor
+    int tam, arg = 0; /*arg = 0 - Valor default*/
+    bool show = true;/*valor default*/
 
-    for(int i = 0; i < tam; i++){
-        cin >> num;
-        vetor.push_back(num);
+    clock_t start_time;
+    double time;
+    
+    ifstream infs;
+
+    switch(argc){
+        case 2:
+            infs.open(argv[1], ifstream::in);
+            break;
+
+        case 3:
+            infs.open(argv[1], ifstream::in);
+            arg = atoi(argv[2]);            
+            break;
+
+        case 4:
+
+            infs.open(argv[1], ifstream::in);
+            arg = atoi(argv[2]);
+            string opt_show(argv[3]);
+            
+            if(!opt_show.compare("false") || !opt_show.compare("f") || !opt_show.compare("0"))
+                show = false;
+            else
+                show = true;
+
     }
 
-   /*exibe_lista(vetor); //Caso use o scrip exec.py, terá que descomentar este trecho e comentar o trecho acima
-    ifstream isfile ("example.in");
-    vector<int> vetor;
-    int n;
+    if(infs.is_open()){
+        infs >> tam;
 
-    if(isfile.is_open()){
-        while(!isfile.eof()){
-            isfile >> n;
-            vetor.push_back(n);
-        }
-        isfile.close();
     }else{
-        cout << "Nao foi possivel abrir o arquivo" << endl;
+
+        cin >> tam;
+
     }
 
-    vector<int> vetor_insert = vetor;
-    vector<int> vetor_select = vetor;
-    vector<int> vetor_quick = vetor;
-    vector<int> vetor_merge = vetor;
-    vector<int> vetor_heap = vetor;
-    */
+    int vetor[tam], vetor_insert[tam], vetor_select[tam], 
+        vetor_quick[tam], vetor_merge[tam], vetor_heap[tam];
 
-    int arg = atoi(argv[1]);
+    if(infs.is_open()){
+
+        for(int i = 0; i < tam; i++){
+
+            infs >> vetor[i];
+
+            vetor_insert[i] = vetor_select[i] = vetor[i];
+            vetor_quick[i] = vetor_merge[i] = vetor_heap[i] = vetor[i];
+
+        }
+
+    }else{
+        for(int i = 0; i < tam; i++){
+            
+            cin >> vetor[i];
+
+            vetor_insert[i] = vetor_select[i] = vetor[i];
+            vetor_quick[i] = vetor_merge[i] = vetor_heap[i] = vetor[i];
+
+        }
+    }
+
+    if(show) { exibe_lista(vetor, tam); }
 
     switch(arg){
+
         case 1:
 
-            insertion_sort(vetor);
+            start_time = clock();
+            insertion_sort(vetor_insert, tam);
+            time = (double)(clock() - start_time)/CLOCKS_PER_SEC;
 
-            if(ordenado(vetor)){
+            if(ordenado(vetor_insert, tam)){
                 cout << "vetor insertion" << endl;
-                exibe_lista(vetor);
+                if(show) exibe_lista(vetor_insert, tam);
+                osfile << time << ", 0, 0, 0, 0"; /**"Vetor insertion - Tempo decorrido(s): "*/
             }
 
             break;
 
         case 2:
 
-            selection_sort(vetor);
+            start_time = clock();
+            selection_sort(vetor_select, tam);
+            time = (double)(clock() - start_time)/CLOCKS_PER_SEC;
 
-            if(ordenado(vetor)) {
-                //cout << "vetor selection" << endl;
-                exibe_lista(vetor);
+            if(ordenado(vetor_select, tam)) {
+                cout << "vetor selection" << endl;
+                if(show) exibe_lista(vetor_select, tam);
+                osfile << "0, " << time << ", 0, 0, 0";
             }
 
             break;
 
         case 3:
 
-            quick_sort(vetor, 0, vetor.size()-1);
+            start_time = clock();
+            quick_sort(vetor_quick, 0, tam-1);
+            time = (double)(clock() - start_time)/CLOCKS_PER_SEC;
 
-            if(ordenado(vetor)) {
-                //cout << "vetor quick" << endl;
-                exibe_lista(vetor);
+            if(ordenado(vetor_quick, tam)) {
+                cout << "vetor quick" << endl;
+                if(show) exibe_lista(vetor_quick, tam);
+                osfile << "0, 0, " << time << ", 0, 0";
             }
 
             break;
 
         case 4:
 
-            merge_sort(vetor, 0, vetor.size()-1);
+            start_time = clock();
+            merge_sort(vetor_merge, tam, 0, tam-1);
+            time = (double)(clock() - start_time)/CLOCKS_PER_SEC;
 
-            if(ordenado(vetor)) {
-                //cout << "vetor merge" << endl;
-                exibe_lista(vetor);
+            if(ordenado(vetor_merge, tam)) {
+                cout << "vetor merge" << endl;
+                if(show) exibe_lista(vetor_merge, tam);
+                osfile << "0, 0, 0, "<< time << ", 0";
             }
 
             break;
 
         case 5:
 
-            heap_sort(vetor);
+            start_time = clock();
+            heap_sort(vetor_heap, tam);
+            time = (double)(clock() - start_time)/CLOCKS_PER_SEC;
 
-            if(ordenado(vetor)) {
-                //cout << "vetor heap" << endl;
-                exibe_lista(vetor);
+            if(ordenado(vetor_heap, tam)) {
+                cout << "vetor heap" << endl;
+                if(show) exibe_lista(vetor_heap, tam);
+                osfile << "0, 0, 0, 0, " << time;
+            }
+
+            break;
+
+        case 0:
+
+            start_time = clock();
+            insertion_sort(vetor_insert, tam);
+            time = (double)(clock() - start_time)/CLOCKS_PER_SEC;
+
+            if(ordenado(vetor_insert, tam)){
+                cout << "vetor insertion" << endl;
+                if(show) exibe_lista(vetor_insert, tam);
+                osfile << time << ", ";;
+            }
+
+            start_time = clock();
+            selection_sort(vetor_select, tam);
+            time = (double)(clock() - start_time)/CLOCKS_PER_SEC;
+
+            if(ordenado(vetor_select, tam)) {
+                cout << "vetor selection" << endl;
+                if(show) exibe_lista(vetor_select, tam);
+                osfile << time << ", ";
+            }
+
+            start_time = clock();
+            quick_sort(vetor_quick, 0, tam-1);
+            time = (double)(clock() - start_time)/CLOCKS_PER_SEC;
+
+            if(ordenado(vetor_quick, tam)) {
+                cout << "vetor quick" << endl;
+                if(show) exibe_lista(vetor_quick, tam);
+                osfile << time << ", ";
+            }
+
+            start_time = clock();
+            merge_sort(vetor_merge, tam, 0, tam-1);
+            time = (double)(clock() - start_time)/CLOCKS_PER_SEC;
+
+            if(ordenado(vetor_merge, tam)) {
+                cout << "vetor merge" << endl;
+                if(show) exibe_lista(vetor_merge, tam);
+                osfile << time << ", ";
+            }
+
+            start_time = clock();
+            heap_sort(vetor_heap, tam);
+            time = (double)(clock() - start_time)/CLOCKS_PER_SEC;
+
+            if(ordenado(vetor_heap, tam)) {
+                cout << "vetor heap" << endl;
+                if(show) exibe_lista(vetor_heap, tam);
+                osfile << time;
             }
 
             break;
@@ -123,18 +227,23 @@ int main(int argc, char* argv[]) {
 
     }
 
+    osfile << "\n";
+
+    infs.close();
+    osfile.close();
+
     return 0;
 }
 
-void insertion_sort(vector<int>& vetor){ /**Metodo Insertion sort**/
+void insertion_sort(int vetor[], int size){ /**Metodo Insertion sort**/
 
-    if(vetor.size() <= 1){ /**Verifica o tamanho do vector**/
+    if(size <= 1){ /**Verifica o tamanho do vector**/
         return ;
     }
 
     int key;
 
-    for(int j = 1; j < vetor.size(); j++){
+    for(int j = 1; j < size; j++){
         key = vetor[j];
         int i = j - 1;
         while(vetor[i] > key && i >=0){
@@ -148,14 +257,14 @@ void insertion_sort(vector<int>& vetor){ /**Metodo Insertion sort**/
 
 }
 
-void selection_sort(vector<int>& vetor){
+void selection_sort(int vetor[], int size){
 
-    int min, aux, n = vetor.size();
+    int min, aux;
 
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < size; i++){
         min = i;
 
-        for(int j = i + 1; j < n; j++){
+        for(int j = i + 1; j < size; j++){
             if(vetor[j] < vetor[min]){
                 min = j;
             }
@@ -170,7 +279,7 @@ void selection_sort(vector<int>& vetor){
 
 }
 
-void quick_sort(vector<int>& vetor, int ini, int fim){ /**Metodo quick sort com o pivo na posicao central do array*/
+void quick_sort(int vetor[], int ini, int fim){ /**Metodo quick sort com o pivo na posicao central do array*/
 
     if(ini >= fim){
         return ;
@@ -248,19 +357,19 @@ void quick_sort(vector<int>& vetor, int ini, int fim){ /**Metodo quick sort com 
     return ;
 }
 
-void merge_sort(vector<int>& vetor, int ini, int fim){
+void merge_sort(int vetor[], int size, int ini, int fim){
 
     if(ini < fim){
         int meio = (fim + ini)/2;
-        merge_sort(vetor, ini, meio);
-        merge_sort(vetor, meio + 1, fim);
-        merge(vetor, ini, meio, fim);
+        merge_sort(vetor, size, ini, meio);
+        merge_sort(vetor, size, meio + 1, fim);
+        merge(vetor, size, ini, meio, fim);
     }
 }
 
-void merge(vector<int>& vetor, int ini, int meio, int fim){
+void merge(int vetor[], int size, int ini, int meio, int fim){
 
-    int aux[vetor.size()];
+    int aux[size];
 
     for(int i  = ini; i <= fim; i++){
         aux[i] = vetor[i];
@@ -293,8 +402,8 @@ void merge(vector<int>& vetor, int ini, int meio, int fim){
     }
 }
 
-void heap_sort(vector<int>& vetor){
-    int n = vetor.size() - 1, aux;
+void heap_sort(int vetor[], int size){
+    int n = size - 1, aux;
 
     for(int i = n/2; i >= 0; i--){
         max_heap(vetor, i, n);
@@ -309,7 +418,7 @@ void heap_sort(vector<int>& vetor){
     }
 }
 
-void max_heap(vector<int>& vetor, int ind, int n){
+void max_heap(int vetor[], int ind, int n){
     int max = ind, aux;
     int left = ind * 2;
     int right = ind * 2 + 1;
@@ -330,20 +439,16 @@ void max_heap(vector<int>& vetor, int ind, int n){
     }
 }
 
-void exibe_lista(vector<int> vetor){
-
-    int size = vetor.size();
+void exibe_lista(int vetor[], int size){
 
     for(int i = 0; i < size; i++){
-        //cout << "Elemento[" << i << "]: " << vetor[i] << endl;
-        cout << vetor[i] << endl;
+        cout << "Elemento[" << i << "]: " << vetor[i] << endl;
+        //cout << vetor[i] << endl;
     }
     cout << endl;
 }
 
-bool ordenado(vector<int> vetor){
-
-    int size = vetor.size();
+bool ordenado(int vetor[], int size){
 
     for(int i = 0; i < size - 1; i++){
         if(vetor[i] > vetor[i+1]){
--- exec.py
+++ exec.py
@@ -1,13 +1,32 @@
 import subprocess as sp
-import shlex
+from glob import glob
+import sys
 
-print("Escolha o algoritmo de Ordenacao: \n" + "(1) - Insertion\n" + "(2) - Selecion\n" + "(3) - Quick\n" + "(4) - Merge\n" + "(5) - Heap : ")
+if (len(sys.argv) == 1):
+	filepath = ""
+else:
+	filepath = str(sys.argv[1])
+
+sp.call(["make"])
+
+print("Escolha o algoritmo de Ordenacao: \n" + "(0) - Todos\n" + "(1) - Insertion\n" + "(2) - Selecion\n" + "(3) - Quick\n" + "(4) - Merge\n" + "(5) - Heap : ")
 
 arg = input()
 
-sp.call(["make"])
-#sp.call(["./algsort", str(arg)])
-cmd = "./algsort " + str(arg) + " < example.in > saida.txt"
-args = shlex.split(cmd)
-print (args)
-sp.Popen(args)
+print("Mostrar vetores? [S/N]")
+
+arg_show = input()
+
+if(arg_show == 'n' or arg_show == 'N'):
+	arg_show = 'f';
+else:
+	arg_show = '1';
+
+fnames = glob(filepath + '*.in')
+fnames.sort()
+
+for filename in fnames:
+	print (filename)
+	sp.call(["./algsort",filename, str(arg), arg_show])
+
+
